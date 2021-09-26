2b46f9a32afb8103d683413d919a2a317d9b3188
本馬遼太郎 <rajiku.blanc@gmail.com>
2021-09-26 12:28:41 +0900

    meta情報

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -10,7 +10,20 @@ export default function Layout({ children }: PropsWithChildren<any>) {
   return (
     <>
       <Head>
+        <meta httpEquiv='Content-Type' content='text/html; charset=UTF-8' />
+        <meta httpEquiv='content-language' content='ja' />
         <title>Blanc My Portfolio</title>
+        <meta name='description' content='ポートフォリオ' />
+        <meta name='keywords' content='portfolio' />
+        <meta property='og:title' content='Blanc My Portfolio' />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://next-portfolio-theta-lake.vercel.app/' />
+        <meta property='og:description' content='ポートフォリオ' />
+        <meta property='og:site_name' content='Blanc My Portfolio' />
+        <link
+          rel='icon'
+          href='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/whale_1f40b.png'
+        />
       </Head>
       <GlobalStyles />
 
